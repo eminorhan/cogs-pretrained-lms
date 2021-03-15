5fa96439c1234a14e831c18bb69c1e4514415730
eminorhan <eo41@nyu.edu>
2021-03-14 22:44:23 -0400

    add --predict_with_generate, otherwise I get oom errors

--- run_seq2seq.sh
+++ run_seq2seq.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --gres=gpu:v100:1
-#SBATCH --mem=180GB
+#SBATCH --mem=250GB
 #SBATCH --time=48:00:00
 #SBATCH --array=0
 #SBATCH --job-name=t5_cogs
@@ -13,7 +13,7 @@ module purge
 module load cuda-10.1
 
 python -u /misc/vlgscratch4/LakeGroup/emin/cogs-pretrained-lms/run_seq2seq.py \
-    --model_name_or_path t5-base \
+    --model_name_or_path t5-small \
     --use_pretrained_weights False \
     --do_train \
     --do_predict \
@@ -22,11 +22,12 @@ python -u /misc/vlgscratch4/LakeGroup/emin/cogs-pretrained-lms/run_seq2seq.py \
     --train_file data/train.json \
     --test_file data/gen.json \
     --output_dir tmp_t5_small \
-    --per_device_train_batch_size 64 \
-    --per_device_eval_batch_size 64 \
+    --per_device_train_batch_size 72 \
+    --per_device_eval_batch_size 72 \
     --overwrite_output_dir \
     --save_steps 250000 \
     --max_target_length 2000 \
-    --num_train_epochs 100
+    --num_train_epochs 100 \
+    --predict_with_generate
 
 echo "Done"
