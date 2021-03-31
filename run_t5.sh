41b21c40d9bd00d355ce07dd658e84b152356571
eminorhan <eo41@nyu.edu>
2021-03-31 16:50:02 -0400

    commit

--- run_t5.sh
+++ run_t5.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
-#SBATCH --gres=gpu:titanrtx:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --mem=180GB
 #SBATCH --time=48:00:00
 #SBATCH --array=0
@@ -24,12 +24,12 @@ python -u /misc/vlgscratch4/LakeGroup/emin/cogs-pretrained-lms/run_translation.p
     --train_file data/train.json \
     --test_file data/gen.json \
     --output_dir tmp_t5_small_pre \
-    --per_device_train_batch_size 72 \
-    --per_device_eval_batch_size 72 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
     --overwrite_output_dir \
     --save_steps 250000 \
     --max_target_length 2000 \
-    --num_train_epochs 100 \
+    --num_train_epochs 50 \
     --predict_with_generate
 
 echo "Done"
